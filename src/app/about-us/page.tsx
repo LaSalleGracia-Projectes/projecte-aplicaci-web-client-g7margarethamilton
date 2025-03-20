ea6786be68aecd99f5c368525924988c900db694
Admin <jhiguerasru@gmail.com>
2025-03-20 16:45:41 +0100

    fix: :bug: Arreglando bug de "Hydration"
    "Hydration failed because the server-rendered HTML didn't match the client", he usado dynamic de Next para cargar los componentes de framer motion solo en el lado del cliente para evitar que lo renderice el server y para que no salga el error de hydration

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,7 +1,23 @@
 "use client";
 
-import { motion } from "framer-motion";
+//cargar componentes de Framer motion solo en el cliente
 import Header from "@/components/ui/header";
+import dynamic from "next/dynamic";
+
+const MotionH1 = dynamic(
+    () => import("framer-motion").then((mod) => mod.motion.h1),
+    { ssr: false }
+    );
+
+    const MotionH2 = dynamic(
+    () => import("framer-motion").then((mod) => mod.motion.h2),
+    { ssr: false }
+);
+
+    const MotionP = dynamic(
+    () => import("framer-motion").then((mod) => mod.motion.p),
+    { ssr: false }
+);
 
 export default function AboutUsPage() {
     return (
@@ -9,22 +25,31 @@ export default function AboutUsPage() {
         <Header />
         <div className="flex-1 container mx-auto px-4 py-12 flex flex-col md:flex-row gap-8">
         <div className="w-full md:w-1/2">
-            <motion.h1
+            <MotionH1
             initial={{ opacity: 0, y: -20 }} 
             animate={{ opacity: 1, y: 0 }} 
-            transition={{ duration: 0.5 }} 
+            transition={{ duration: 0.4 }} 
             className="text-4xl font-bold mb-6"
         >
-            Sobre Nosotros
-        </motion.h1>
-        <motion.p
+            Flow2Day!
+        </MotionH1>
+
+        <MotionH2 initial={{ opacity: 0, y: -20 }} 
+            animate={{ opacity: 1, y: 0 }} 
+            transition={{ duration: 0.6 }} 
+            className="text-2xl font-bold mb-6"
+        >
+            Sobre nosotros
+        </MotionH2>
+
+        <MotionP
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.3, duration: 0.8 }}
             className="text-lg text-muted-foreground"
         >
             Flow2Day es una aplicación web que te ayuda a organizar tus tareas y alcanzar tus metas.
-        </motion.p>
+        </MotionP>
         </div>
 
 
@@ -36,6 +61,11 @@ export default function AboutUsPage() {
             </div>
         </div>
         </div>
+        <footer className="bg-secondary py-8 text-center">
+        <p className="text-sm text-muted-foreground">
+            Hecho con ❤️ y Next.js | © {new Date().getFullYear()} Flow2Day
+        </p>
+        </footer>
     </div>
     );
 }
